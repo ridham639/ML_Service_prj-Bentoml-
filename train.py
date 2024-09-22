f29d40426ab6b1fd8458aa37c11011772bedd441
ridham639 <rythmsharma2007@gmail.com>
2024-09-22 17:39:08 +0530

    bentoml files

---
+++ train.py
@@ -0,0 +1,17 @@
+import bentoml
+
+from sklearn import svm
+from sklearn import datasets
+
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+
+clf = svm.SVC(gamma='scale')
+clf.fit(X, y)
+
+saved_model = bentoml.sklearn.save_model("iris_clf", clf)
+print(f"Model saved: {saved_model}")
+
+## tag="iris_clf:go4zgudyz6ubbgfp 
